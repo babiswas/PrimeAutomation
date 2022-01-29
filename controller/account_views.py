907f4bc675c23a725dae88d4b51afc00ed1092ac
Bapan Biswas <babiswas@adobe.com>
2022-01-29 13:39:41 +0530

    commited

--- controller/account_views.py
+++ controller/account_views.py
@@ -1,25 +1,32 @@
-from flask import jsonify
 from Model import db
 from Model.Testmodel import Account
 from Route.route import account
 from flask import request
-from forms.forms import TestForm
+from forms.forms import AccountForm
 from flask import redirect
 from flask import url_for
 from flask import render_template
 
 
 @account.route('/add',methods=['GET','POST'])
-def add_create_tests():
+def add_account():
       accountform=AccountForm(request.form)
       if request.method=="POST" and accountform.validate:
-         testid=request.form.get("id")
-         testtag=request.form.get("tags")
-         testdescription=request.form.get("description")
-         testresource=request.form.get("resourcePath")
-         method=request.form.get("Method")
-         account=Account(testid,testtag,testdescription,testresource,method)
+         accountid=request.form.get("id")
+         clientid=request.form.get("tags")
+         clientsecret=request.form.get("description")
+         refreshtoken=request.form.get("resourcePath")
+         account=Account(accountid,clientid,clientsecret,refreshtoken)
          db.session.add(account)
          db.session.commit()
          return redirect(url_for("account.read_all"))
       return render_template("add_account.html",form=accountform)
+
+
+@account.route('/read',methods=['GET'])
+def account_list():
+      page=request.args.get('page',1,type=int)
+      accountlist=Account.query.paginate(page=page,per_page=5)
+      next_url=url_for('account.account_list',page=accountlist.next_num) if accountlist.has_next else None
+      prev_url=url_for('account.account_list',page=accountlist.prev_num) if accountlist.has_prev else None
+      return render_template('account_list.html',accountlist=accountlist,next_url=next_url,prev_url=prev_url)
