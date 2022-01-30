04ea61ff43d12d33488a2293e218b6654d59f3ba
Bapan Biswas <babiswas@adobe.com>
2022-01-31 00:18:16 +0530

    commited

--- controller/test_views.py
+++ controller/test_views.py
@@ -15,21 +15,18 @@ def add_create_tests(envid,accountid):
          testtag=request.form.get("tags")
          testdescription=request.form.get("description")
          testresource=request.form.get("resourcePath")
-         method=request.form.get("Method")
-         testcase=Testcase(testid,testtag,testdescription,testresource,method,accountid,envid)
+         mthd=request.form.get("method")
+         testcase=Testcase(testid,testtag,testdescription,testresource,mthd,accountid,envid)
          db.session.add(testcase)
          db.session.commit()
-         return redirect(url_for("testcase.test_list"))
+         return redirect(url_for("testcase.test_list",accountid=accountid))
       return render_template("add_test.html",form=testform)
 
 
-@testcase.route('/read',methods=['GET'])
-def test_list():
-      page=request.args.get('page',1,type=int)
-      testlist=Testcase.query.paginate(page=page,per_page=5)
-      next_url=url_for('testcase.test_list',page=testlist.next_num) if testlist.has_next else None
-      prev_url=url_for('testcase.test_list',page=testlist.prev_num) if testlist.has_prev else None
-      return render_template('test_list.html',testlist=testlist,next_url=next_url,prev_url=prev_url)
+@testcase.route('/<int:accountid>/read',methods=['GET'])
+def test_list(accountid):
+      testlist=Testcase.query.filter_by(acc_id=accountid).all()
+      return render_template('test_list.html',testlist=testlist)
 
 
 @testcase.route('/edit/<int:testid>',methods=['GET','POST'])
